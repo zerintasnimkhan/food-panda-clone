3436dde3d41935090bc00298dbe95255d96a6f07
zerintasnimkhan <zerintasnimriana@gmail.com>
2023-11-13 14:58:30 +0600

    refactor: removed typescript from client

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl font-bold underline">
+      Food Panda!
+    </h1>
+    <button className='btn btn-primary'>Click me!</button>
+    </>
+  )
+}
+
+export default App
