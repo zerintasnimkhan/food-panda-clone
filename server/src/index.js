36cc7da7be31712c0352ab1c640f712dff302db1
zerintasnimkhan <zerintasnimriana@gmail.com>
2023-11-17 18:35:33 +0600

    fix: login

--- server/src/index.js
+++ server/src/index.js
@@ -1,43 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const config = require('./config');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const config = require("./config");
 const bodyParser = require("body-parser");
-const categoryRouter = require('./routers/category.router');
-const orderRouter = require('./routers/order.router');
-const restaurantRouter = require('./routers/restaurant.router');
-const foodRouter = require('./routers/food.router')
-const Router = require('./routers/router');
-const loginrouter = require('./routers/router');
-const registerrouter = require('./routers/router');
-const registerRestaurant=require('./routers/router')
+const categoryRouter = require("./routers/category.router");
+const orderRouter = require("./routers/order.router");
+const restaurantRouter = require("./routers/restaurant.router");
+const foodRouter = require("./routers/food.router");
+const Router = require("./routers/router");
+const loginrouter = require("./routers/router");
+const registerrouter = require("./routers/router");
+const registerRestaurant = require("./routers/router");
 const router = express.Router();
 const app = express();
 
 app.use(cors());
-app.use(bodyParser);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/', Router);
+app.use("/", Router);
 
-app.use('/login', loginrouter);
-app.use('/register', registerrouter);
-app.use('/resisterRestaurant',registerRestaurant)
-
-
-app.use('/category', categoryRouter);
-app.use('/order', orderRouter);
-app.use('/restaurant', restaurantRouter);
-app.use('/food', foodRouter);
+app.use("/category", categoryRouter);
+app.use("/order", orderRouter);
+app.use("/restaurant", restaurantRouter);
+app.use("/food", foodRouter);
 
 (async function () {
-      try {
-            await mongoose.connect(config.MONGOOSE_URI);
-            console.log('[mongoose]: Connected to DB.');
-            app.listen(config.PORT, () => console.log(`[server]: Server is listening on port ${config.PORT}`));
-      } catch (error) {
-            console.log(error);
-      }
+  try {
+    await mongoose.connect(config.MONGOOSE_URI);
+    console.log("[mongoose]: Connected to DB.");
+    app.listen(config.PORT, () =>
+      console.log(`[server]: Server is listening on port ${config.PORT}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
 })();
-
-
