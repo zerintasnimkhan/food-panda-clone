b28334a6244a703b4ed74f80eb93c6fb69f59bcd
Samiya Kazi <samiya.kazi09@gmail.com>
2023-11-18 11:43:01 +0600

    fix: daisyui modal

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
+import { Modal, Button } from 'react-daisyui'
+import LoginModal from "./LoginModal";
 
 const AppNavbar = () => {
+  const loginRef = useRef<HTMLDialogElement>(null);
+  const handleShow = useCallback(() => {
+    loginRef.current?.showModal();
+  }, [loginRef]);
+
+
   return (
     <div className="navbar bg-accent shadow-2xl">
       <div className="flex-1">
@@ -9,7 +17,7 @@ const AppNavbar = () => {
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1 text-primary text-xl pr-20">
           <li>
-            <a>Log in</a>
+            <a onClick={handleShow}>Log in</a>
           </li>
           <li>
             <a>Sign up</a>
@@ -18,7 +26,7 @@ const AppNavbar = () => {
             <details>
               <summary>EN</summary>
               <ul className="bg-accent p-2">
-                <li> 
+                <li>
                   <a>Link 1</a>
                 </li>
                 <li>
@@ -29,6 +37,18 @@ const AppNavbar = () => {
           </li>
         </ul>
       </div>
+
+      <Modal ref={loginRef}>
+        <Modal.Header className="font-bold">Login to FoodPanda!</Modal.Header>
+        <Modal.Body>
+          <LoginModal />
+        </Modal.Body>
+        <Modal.Actions>
+          <form method="dialog">
+            <Button>Close</Button>
+          </form>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 };
