308948a622bd2126f55accc9255790d83953fbaf
Samiya Kazi <samiya.kazi09@gmail.com>
2023-11-20 21:44:42 +0600

    feat: fixed restaurant info page

---
+++ client/src/Pages/Restaurant/FoodPage.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import { getRestaurantFood } from "../../services/restaurant.service";
+
+const FoodsPage = ({ restaurantId }) => {
+  const [foods, setFoods] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchFoods = async () => {
+      try {
+        const foodsData = await getRestaurantFood(restaurantId);
+        setFoods(foodsData);
+      } catch (error) {
+        console.error("Error in FoodsPage:", error);
+        setError(error.message);
+      }
+    };
+
+    fetchFoods();
+  }, [restaurantId]);
+
+  return (
+    <div>
+      <h1>Foods for your Restaurant:</h1>
+      <br />
+
+      {error && <p>Error: {error}</p>}
+
+      {foods.length === 0 ? (
+        <p>No foods found for this restaurant.</p>
+      ) : (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {foods.map((food) => (
+            <div
+              key={restaurantId.food._id}
+              className="p-4 border rounded-md shadow-md"
+            >
+              <h2 className="text-xl font-bold">{restaurantId.food.name}</h2>
+              <p>Price: {food}</p>
+              <p>Category: {food}</p>
+              <p>Prepare Time: {food}</p>
+              <p>Serving Size: {food}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FoodsPage;
--- client/src/Pages/Restaurant/RestaurantInfo.jsx
+++ client/src/Pages/Restaurant/RestaurantInfo.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { restaurantInfo } from '../../services/restaurant.service';
-import { useNavigate } from 'react-router-dom';
-import RestaurantMap from '../../components/RestaurantMap';
+import React, { useEffect, useState } from "react";
+import { restaurantInfo } from "../../services/restaurant.service";
+import { useNavigate } from "react-router-dom";
+import RestaurantMap from "../../components/RestaurantMap";
 
 const RestaurantList = () => {
   const [restaurants, setRestaurants] = useState([]);
@@ -15,7 +15,7 @@ const RestaurantList = () => {
         const restaurantData = await restaurantInfo();
         setRestaurants(restaurantData);
       } catch (error) {
-        console.error('Error in RestaurantList:', error);
+        console.error("Error in RestaurantList:", error);
         setError(error.message);
       }
     };
@@ -25,46 +25,64 @@ const RestaurantList = () => {
 
   return (
     <>
-      {!!restaurants.length &&
+      {!!restaurants.length && (
         <div className="flex-1">
-          <div className="hero h-52" style={{ backgroundImage: `url(${restaurants[0].imgUrl})` }}>
+          <div
+            className="hero h-52"
+            style={{ backgroundImage: `url(${restaurants[0].imgUrl})` }}
+          >
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
               <div className="max-w-md">
-                <h1 className="mb-5 text-5xl font-bold">{restaurants[0].name}</h1>
+                <h1 className="mb-5 text-5xl font-bold">
+                  {restaurants[0].name}
+                </h1>
               </div>
             </div>
           </div>
-          <div className='flex w-full justify-around mt-5 text-lg'>
+          <div className="flex w-full justify-around mt-5 text-lg">
             <div>
-              <div className='my-5'><b>Restaurant name:</b> {restaurants[0].name}</div>
+              <div className="my-5">
+                <b>Restaurant name:</b> {restaurants[0].name}
+              </div>
 
-              <div className='flex my-5'>
-                <div><b>Address:</b></div>
-                <div className='ml-5'>
+              <div className="flex my-5">
+                <div>
+                  <b>Address:</b>
+                </div>
+                <div className="ml-5">
                   <div>{restaurants[0].address.street}</div>
                   <div>{restaurants[0].address.city}</div>
                   <div>{restaurants[0].address.district}</div>
                 </div>
               </div>
 
-              <div className='my-5'>
-                <b>Categories:</b> {restaurants[0].categories.map(category =>
-                  <div key={category} className="badge badge-primary">{category.name}</div>
-                )}
+              <div className="my-5">
+                <b>Categories:</b>{" "}
+                {restaurants[0].categories.map((category) => (
+                  <div key={category} className="badge badge-primary">
+                    {category.name}
+                  </div>
+                ))}
               </div>
 
-              <button className='btn btn-primary' onClick={() => {navigate('/restaurant/edit/info', { state: { restaurant: restaurants[0] } })}}>Edit Information</button>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  navigate("/restaurant/edit/info", {
+                    state: { restaurant: restaurants[0] },
+                  });
+                }}
+              >
+                Edit Information
+              </button>
             </div>
             <RestaurantMap restaurant={restaurants[0]} />
           </div>
         </div>
-      }
+      )}
     </>
   );
 };
 
-
 export default RestaurantList;
-
-
